808c7d4f752b5dcdb426842e05fb81ee16b884f3
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-06-30 15:04:40 +0200

    Input form

--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import MeetingForm
+
 from .models import Meeting, Room
 
 # Create your views here.
@@ -9,3 +11,13 @@ def detail(request, id):
 def rooms_list(request):
     return render(request, "meetings/rooms_list.html",
                   {"rooms": Room.objects.all()})
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
